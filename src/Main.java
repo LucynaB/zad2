da49387e7d2f6dd8b5963234ab2acf010916548c
lblaszczyk <lusia22b@wp.pl>
2018-07-06 10:41:30 +0200

    krok 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Telewizor tv1 = new Telewizor();
-        tv1.producent = "Samsung";
-        tv1.year = 2013;
-        tv1.inch = 43.5;
+        Telewizor tv1 = new Telewizor("Samsung", 2013, 43.5);
+//        tv1.producent = "Samsung";
+//        tv1.year = 2013;
+//        tv1.inch = 43.5;
 
-        Telewizor tv2 = new Telewizor();
-        tv2.producent = "Sony";
-        tv2.year = 2018;
-        tv2.inch = 65.0;
+        Telewizor tv2 = new Telewizor("Sony", 2018, 65);
+//        tv2.producent = "Sony";
+//        tv2.year = 2018;
+//        tv2.inch = 65.0;
 
         System.out.println("Telewizor 1: "+tv1.producent+" "+tv1.year+" "+tv1.inch);
         System.out.println("Telewizor 2: "+tv2.producent+" "+tv2.year+" "+tv2.inch);
--- src/Telewizor.java
+++ src/Telewizor.java
@@ -2,4 +2,10 @@ public class Telewizor {
     String producent;
     int year;
     double inch;
+
+    Telewizor(String p, int y, double i){
+        producent = p;
+        year = y;
+        inch = i;
+    }
 }
